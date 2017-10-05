976ff3beda38ebdc30617e296b5ca48b990d36d9
jochen <jochen@netlog.com>
2017-10-05 14:33:58 +0200

    phpunit

--- tests/TestingBotTest.php
+++ tests/TestingBotTest.php
@@ -1,4 +1,10 @@
 <?php
+	
+	if (!class_exists('\PHPUnit\Framework\TestCase', true)) {
+	    class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
+	} elseif (!class_exists('\PHPUnit_Framework_TestCase', true)) {
+	    class_alias('\PHPUnit\Framework\TestCase', '\PHPUnit_Framework_TestCase');
+	}
 
 	require_once('src/TestingBot/TestingBotAPI.php');
 
